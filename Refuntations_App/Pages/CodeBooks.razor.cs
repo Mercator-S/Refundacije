63b7c58d8da68931936e5e0f1ecd20e21857cc8f
DraganaMercator <dragana.stefanovic@mercator.rs>
2022-12-06 10:10:35 +0100

    CodeBook component is initialized.

---
+++ Refuntations_App/Pages/CodeBooks.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Refuntations_App.Pages
+{
+    partial class CodeBooks
+    {
+        private List<string> Columns { get; set; } = null;
+        private List<object> Elements { get; set; } = new List<object>();
+        protected override async Task OnInitializedAsync()
+        {
+            Columns = new List<string>();
+            Columns.Add("First");
+            Columns.Add("Second");
+            Columns.Add("Third");
+            Columns.Add("Fourth");
+        }
+
+    }
+}
