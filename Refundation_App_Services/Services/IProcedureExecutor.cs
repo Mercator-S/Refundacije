db0e9f2ed8b23905b76c9be059f632951edc1dd0
DraganaMercator <dragana.stefanovic@mercator.rs>
2022-12-08 15:11:03 +0100

    Gitignore fixed.

--- Refundation_App_Services/Services/IProcedureExecutor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Refuntations_App_Data.Model;
-
-namespace Refundation_App_Services.Services
-{
-    public interface IProcedureExecutor
-    {
-        Task<List<FinalSettlements>> GetFinalSettlement(int Year, int Month);
-    }
-}
