6f85dd9289feab1ccf693526b70f1403a919c815
DraganaMercator <dragana.stefanovic@mercator.rs>
2022-12-08 15:34:57 +0100

    Only necessary files.

---
+++ Refuntations_App/Dialog/FinalSettlementDialog.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Refundation_App_Services.Services;
+
+namespace Refuntations_App.Dialog
+{
+    partial class FinalSettlementDialog
+    {
+        [CascadingParameter]
+        MudDialogInstance? MudDialog { get; set; }
+        [Inject]
+        private IProcedureExecutor? procedureExecutor { get; set; }
+        public int Year { get; set; } = DateTime.Now.Year;
+        public int Month { get; set; } = DateTime.Now.Month - 1;
+
+        private async Task Submit(int Year, int Month)
+        {
+            DialogParameters parameteres = new DialogParameters();
+            var finalSettlements = await Task.Run(() => procedureExecutor.GetFinalSettlement(Year, Month));
+            parameteres.Add("finalSettlements", finalSettlements);
+            MudDialog.Close(DialogResult.Ok(parameteres));
+        }
+
+        void Cancel()
+        {
+            MudDialog.Close();
+        }
+    }
+}
---
+++ Refuntations_App/Program.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using Refundation_App_Services.Repositories;
+using Refundation_App_Services.Services;
+using Refuntations_App.Areas.Identity;
+using Refuntations_App.Data;
+using Refuntations_App_Data.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<OnlineUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 8;
+
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?-._@+";
+    options.User.RequireUniqueEmail = true;
+
+} )
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<OnlineUser>>();
+builder.Services.AddTransient<IProcedureExecutor, ProcedureExecutor>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
