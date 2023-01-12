670d3c69e5cece3214c20d086774ba3501ae8cfc
DraganaMercator <dragana.stefanovic@mercator.rs>
2023-01-12 14:37:23 +0100

    Password is self-generated. Register functionality is invisible for users. Email sending problem is resolved.

--- Refuntations_App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Refuntations_App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,14 +74,6 @@ namespace Refuntations_App.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
             public string Password { get; set; }
 
             /// <summary>
@@ -176,6 +168,18 @@ namespace Refuntations_App.Areas.Identity.Pages.Account
                 throw new NotSupportedException("The default UI requires a user store with email support.");
             }
             return (IUserEmailStore<OnlineUser>)_userStore;
+        }
+        private void GeneratePassword()
+        {
+            Input.Password = Input.Email.Substring(0, Input.Email.IndexOf("."));
+            Input.Password += GetThreeRandomNumbers();
+            Input.Password += "?";
+        }
+        private string GetThreeRandomNumbers()
+        {
+            Random rnd = new Random();
+            string result = rnd.Next(100, 999).ToString();
+            return result;
         }
     }
 }
